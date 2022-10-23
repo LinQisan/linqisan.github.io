b5f2347827e923a80ffcc9dab0c6ffba11f14beb
LinQisan <1986972620@qq.com>
2022-10-23 19:14:38 +0800

    final nav

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-/*
-Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
-编写一个以整数为输入的函数 并返回该数字的二进制表示形式中等于1的位数。输入是非负的
-Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
-*/
-
-// const countBits = function (n) {
-//   // match 方法如果找不到会返回 null
-//   const binary = n.toString(2).match(/1/g)
-//   return binary ? binary.length : 0
+// function startTime() {
+//   const today = new Date();
+//   const h = String(today.getHours()).padStart(2, 0);
+//   const m = String(today.getMinutes()).padStart(2, 0);
+//   const s = String(today.getSeconds()).padStart(2, 0);
+//   document.getElementById('time').innerHTML = `${h}时${m}分${s}秒`
+//   setTimeout(startTime, 500);
 // }
-// eslint-disable-next-line no-undef
-// const time = Vue.createApp({
-//   methods: {
-//     currentTime() {
-//       return `${new Date().getHours()}时${String(new Date().getMinutes()).padStart(2, 0)}分${String(new Date().getSeconds()).padStart(2, 0)}秒`;
-//     },
-//   },
-// })
-// time.mount('.main-header')
-
-function startTime() {
-  let today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
-  let t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-  if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
-  return i;
-}
+// startTime()
+setInterval(function () {
+  const now = new Date()
+  const strTime = function (num) { return String(num).padStart(2, 0) }
+  const h = strTime(now.getHours())
+  const m = strTime(now.getMinutes())
+  const s = strTime(now.getSeconds())
+  document.getElementById('time').innerHTML = `${h}:${m}:${s}`
+}, 250)
 
 const movie = Vue.createApp({
   data() {
@@ -41,24 +22,16 @@ const movie = Vue.createApp({
       film: '千年女优',
       director: '今敏',
       releaseYear: 2002,
-      imgUrl: 'https://arielhsu.tw/wp-content/uploads/20210202194826_70.jpg',
+      imgUrl: 'https://www.themoviedb.org/t/p/original/iVNCVWAqBSDU0MJCq1ehAIGifev.jpg',
       watchTimes: 0,
       name: '',
     };
   },
   methods: {
-    setName(event, text) {
-      this.name = text + event.target.value;
-    },
-    countdown() {
-      return new Date().getFullYear() - this.releaseYear;
-    },
-    addTime(n = 1) {
-      this.watchTimes += n;
-    },
-    reduceTime(n = 1) {
-      this.watchTimes !== 0 ? (this.watchTimes -= n) : 0;
-    },
+    setName(event, text) { this.name = text + event.target.value },
+    countdown() { return new Date().getFullYear() - this.releaseYear },
+    addTime(n = 1) { this.watchTimes += n },
+    reduceTime(n = 1) { this.watchTimes !== 0 ? (this.watchTimes -= n) : 0; },
   },
 });
 movie.mount('#movie');
